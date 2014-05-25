63891417728f4b5c44c446330ca79d2fe8072fb0
Simon Garnier <garnier@njit.edu>
2014-05-25 00:11:46 -0400

    Corrected a bug with the automatic collection of state names. Added a map per capita by state.

--- script.R
+++ script.R
@@ -9,8 +9,9 @@ if (!require("graphZoo")) {
   require("devtools")
   install_github("morpionZ/graphZoo")
 }
-library(extrafont)
+require("extrafont")
 loadfonts()
+require("acs")
 
 
 #+ load.data, echo=FALSE
@@ -46,16 +47,16 @@ if (!file.exists("data/clan-lab.csv")) {
     
     if (any(grepl("No reported seizures", raw))) {} else {
       procd <- as.data.table(raw) %>%
-        mutate(STATE = toupper(word(raw[1], start = -1, end = -1))) %>%
+        mutate(STATE = toupper(gsub("^(.+?)- ", "", raw[1]))) %>%
         filter(raw != "", 
                !grepl("COUNTY\\s{2,}CITY", raw), 
                !grepl("\f", raw),
                !grepl("National Clandestine", raw), 
                !grepl("[0-9] of [0-9]", raw),
                str_count(raw, "\\s{2,}") == 3) %>%
-        mutate(COUNTY = word(raw, start = 1, end = 1, sep = "\\s{2,}"),
-               CITY = word(raw, start = 2, end = 2, sep = "\\s{2,}"),
-               ADDRESS = word(raw, start = 3, end = 3, sep = "\\s{2,}"),
+        mutate(COUNTY = toupper(word(raw, start = 1, end = 1, sep = "\\s{2,}")),
+               CITY = toupper(word(raw, start = 2, end = 2, sep = "\\s{2,}")),
+               ADDRESS = toupper(word(raw, start = 3, end = 3, sep = "\\s{2,}")),
                DATE = word(raw, start = 4, end = 4, sep = "\\s{2,}")) %>%
         mutate(DATE = as.Date(DATE, "%m/%d/%Y")) %>%
         select(-raw)
@@ -93,7 +94,7 @@ if (!file.exists("data/clan-lab.csv")) {
 }
 
 
-#+ plot.map, echo=TRUE
+#+ plot.raw.map, echo=TRUE
 us.state.map <- map_data('state')
 xlim <- range(us.state.map$long)
 xlim <- xlim + diff(xlim) * c(-.05, .05)
@@ -130,6 +131,64 @@ g <- addBanner(g, font.size = 5.83,
 png("clan-lab.png", width = 1200, height = 700, bg = "#F0F0F0")
 g
 dev.off()
+
+
+#+ get.state.pop, echo=FALSE
+tmp <- acs.fetch(geography = geo.make(state = "*"), 
+                        table.number = "B01003")
+state.pop <- data.table(STATE = toupper(geography(tmp)$NAME),
+                        estimate(tmp))
+
+#+ compute.count.data.by.state, echo=FALSE
+count.data <- lab.data %>%
+  group_by(STATE) %>%
+  summarize(COUNT = length(ADDRESS)) %>%
+  merge(state.pop, by = "STATE") %>%
+  mutate(PERCAP = 100000 * (COUNT / B01003_001))
+  
+
+#+ plot.count.data.by.state, echo=TRUE
+us.state.map <- as.data.table(map_data('state')) %>%
+  mutate(STATE = toupper(region)) %>%
+  merge(count.data, by = "STATE", all.x = TRUE)
+  
+xlim <- range(us.state.map$long, na.rm = TRUE)
+xlim <- xlim + diff(xlim) * c(-.05, .05)
+ylim <- range(us.state.map$lat, na.rm = TRUE)
+ylim <- ylim + diff(xlim) * c(-.05, .05)
+subtitle <- paste0("Drug Enforcement Administration (", nrow(lab.data), " locations between ", 
+                   year(min(lab.data$DATE, na.rm = TRUE)), " and ", 
+                   year(max(lab.data$DATE, na.rm = TRUE)), ")")
+
+g <- ggplot() +
+  geom_polygon(data = us.state.map, 
+               aes(x = long, y = lat, group = group, fill = PERCAP),
+               color = "white") +
+  scale_fill_gradient(low = "steelblue4", high = "firebrick4", 
+                      limits = c(0, max(count.data$PERCAP)),
+                      guide = guide_colorbar(title.position = "top",
+                                             label.position = "bottom",
+                                             barwidth = 40)) +
+  coord_fixed(xlim = xlim, ylim = ylim) +
+  theme_graphzoo(base_size = 28, family = "Ume P Gothic") +
+  theme(axis.line = element_blank(), 
+        axis.text.x = element_blank(), 
+        axis.text.y = element_blank(),
+        axis.ticks = element_blank(), 
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        plot.margin = unit(c(1, 0, 0, -1), "lines"),
+        legend.position = "bottom") +
+  ggtitle(bquote(atop("Number of suspected clandestine laboratories per 100,000 inhabitants", 
+                      atop(italic(.(subtitle)), ""))))
+
+g <- addBanner(g, font.size = 5.83,
+               l.txt = "GRAPHZOO.TUMBLR.COM", r.txt = "SOURCE: DRUG ENFORCEMENT ADMINISTRATION, US CENSUS BUREAU")
+
+png("clan-lab_percap.png", width = 1200, height = 700, bg = "#F0F0F0")
+g
+dev.off()
+
 
 
 
